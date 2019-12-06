40dd4679ab5175ca3914d6626d5bd6ce5f9aadd5
Alex Bigelow <alex.r.bigelow@gmail.com>
2019-12-06 14:38:01 -0700

    Debugging odd ordering problem

--- database.py
+++ database.py
@@ -597,16 +597,27 @@ class Database:
 
             # Parent GUIDs refer to the one in the enter event, not the leave event
             guid = intervalObj.get('GUID', intervalObj['enter'].get('GUID', None))
+
             if guid is None:
                 missingCount += 1
-                continue
+            else:
+                if not guid in guids:
+                    guids[guid] = []
+                guids[guid] = guids[guid] + [intervalId]
 
             # Connect to most recent interval with the parent GUID
             parentGuid = intervalObj.get('Parent GUID', intervalObj['enter'].get('Parent GUID', None))
+
+            problemIds = ['325', '826', '1082', '68']
+            if intervalObj['intervalId'] in problemIds:
+                print(intervalObj['intervalId'], guid, guid in guids, parentGuid, parentGuid in guids)
+
             if parentGuid is not None and parentGuid in guids:
                 foundPrior = False
                 for parentIntervalId in reversed(guids[parentGuid]):
                     parentInterval = intervals[parentIntervalId]
+                    if intervalObj['intervalId'] == '826':
+                        print(parentInterval['enter']['Timestamp'], parentInterval['exit']['Timestamp'], intervalObj['enter']['Timestamp'])
                     if parentInterval['enter']['Timestamp'] <= intervalObj['enter']['Timestamp']:
                         foundPrior = True
                         intervalCount += 1
@@ -633,13 +644,6 @@ class Database:
                     missingCount += 1
             else:
                 missingCount += 1
-
-            # Store this interval by its leave GUID
-            guid = intervalObj.get('GUID', intervalObj['leave'].get('GUID', None))
-            if guid is not None:
-                if guid not in guids:
-                    guids[guid] = []
-                guids[guid] = guids[guid] + [intervalId]
 
             if (missingCount + intervalCount) % 2500 == 0:
                 await log('.', end='')
