bfbadf81326e4bb1cc75821fb63c5402e11a963c
sayefsakin <sayefsakin@yahoo.co.in>
2020-02-13 12:56:52 -0700

    profiler input file for configuration

--- profiling_tools/profile_script.py
+++ profiling_tools/profile_script.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import json
 
 import requests
 import argparse
@@ -13,6 +14,8 @@ parser.add_argument('-b', '--begin', dest='begin', default=None,
                     help='Beginning of the range of values to be queried by histogram or interval call.')
 parser.add_argument('-en', '--end', dest='end', default=None,
                     help='End of the range of values to be queried by histogram or interval call.')
+parser.add_argument('-bn', '--bins', dest='bins', default=None,
+                    help='Total number of bins.')
 parser.add_argument('-d', '--datset', dest='dataset_label', default=None,
                     help = 'The name of the dataset we are querying from.')
 parser.add_argument('-s', '--sort_order', dest='sort_order', default='cumulative',
@@ -23,17 +26,28 @@ parser.add_argument('-i', '--input', dest="spec_file", default=None,
                     help="This argument gives the filename of a configuration file which is open and read. The configuration file is .json format.")
 
 def main():
-    endpt = 'http://127.0.0.1:8000'
+    args = vars(parser.parse_args())
+    with open(args['spec_file']) as json_file:
+        data = json.load(json_file)
+        n_trials = data["n_trials"]
+        endpt = data["end_pt"]
+        begin = data["begin"]
+        end = data["end"]
+        bins = data["bins"]
+        dataset_label = data["dataset_label"]
+        sort_order = data["sort_order"]
+        prof_output = data["prof_output"]
 
     # call profile start endpoint
     requests.get(endpt+'/profile/start')
 
+    requestString = endpt + '/profile/datasets/' + dataset_label + '/drawValues/' + str(bins) + '/' + str(begin) + '/' + str(end)
     # loop over number of runs
-    for n in range(0,100):
-        requests.get(endpt+'/profile/datasets/FIB20/drawValues/800/14000000/250000000')
+    for n in range(0, n_trials):
+        requests.get(requestString)
 
     # call profile print endpoint
-    requests.get(endpt+'/profile/print/cumulative/test.prof/{}'.format(n))
+    requests.get(endpt+'/profile/print/' + sort_order + '/' + prof_output + '/{}'.format(n))
 
 
 main()
