f51bb6d1f1a4bfd5984ddff68379d84216a91f35
tapscodes <44383662+tapscodes@users.noreply.github.com>
2023-02-06 10:22:54 -0700

    added check to ensure color exists

--- bundle.py
+++ bundle.py
@@ -134,7 +134,8 @@ async def main():
             db.rename(datasetId, label)
             
             # Assign color
-            db.recolor(datasetId, db[datasetId]['info']['color'])
+            if db[datasetId]['info']['color'] is not None:
+                db.recolor(datasetId, db[datasetId]['info']['color'])
 
             # Assign any tags
             if args['tags'] is not None:
